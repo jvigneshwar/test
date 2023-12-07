ca01884313ab98394ef8f69bb0f77681a92c3304
ShivaShaknr <shivanat05@gmail.com>
2023-12-07 10:28:06 +0530

    scroll-update-2

--- src/components/Scrollable/Scrollable.js
+++ src/components/Scrollable/Scrollable.js
@@ -1,108 +1,43 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState } from 'react'
 import "./Scrollable.css"
+import { useEffect } from 'react'
 
 const Scrollable = ({ section, setSection }) => {
-    const containerRef = useRef(null);
+
+    const [showScroll, setShowScroll] = useState(false)
 
     useEffect(() => {
-        const debounce = (func, delay, immediate) => {
-            let timeoutId;
+        if (section === 0)
+            setShowScroll(true)
+    }, [section])
 
-            return function (...args) {
-                const context = this;
-
-                const later = () => {
-                    timeoutId = null;
-                    if (!immediate) {
-                        func.apply(context, args);
-                    }
-                };
-
-                const callNow = immediate && !timeoutId;
-
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-
-                timeoutId = setTimeout(later, delay);
-
-                if (callNow) {
-                    func.apply(context, args);
-                }
-            };
-        };
-
-        const handleWheel = debounce((event) => {
-            if (event.deltaY <= 0) {
-                if(section>0) 
-                    setSection(pre => pre - 1)
+    const handleScroll = (e) => {
+        console.log(e.target.scrollTop);
+        if (e.target.scrollTop === 0) {
+            if (section > 0) {
+                setSection(pre => pre - 1)
+                setTimeout(() => {
+                    e.target.scrollTop = 3;
+                }, 2000)
             }
-            else {
-                if(section<6)
-                    setSection(pre => pre + 1)
+        } else if (e.target.scrollTop === 6) {
+            if (section < 6) {
+                setSection(pre => pre + 1)
+                setTimeout(() => {
+                    e.target.scrollTop = 3;
+                }, 2000)
             }
-        }, 300, true);
+        }
 
-        const container = containerRef.current;
-
-        container.addEventListener('wheel', handleWheel);
-
-        return () => {
-            container.removeEventListener('wheel', handleWheel);
-        };
-    },[]);
+    }
 
     return (
-        <div
-            ref={containerRef}
-            className='scrollable-div'
-        ></div>
-    );
-};
+        <>
+            <div className='scrollable-div' onScroll={(e) => { handleScroll(e) }}>
+                {showScroll ? <div className='scrollable'></div> : <></>}
+            </div>
+        </>
+    )
+}
 
-export default Scrollable;
-
-
-
-// import React, { useState } from 'react'
-// import "./Scrollable.css"
-// import { useEffect } from 'react'
-
-// const Scrollable = ({ section, setSection }) => {
-
-//     const [showScroll, setShowScroll] = useState(false)
-
-//     useEffect(() => {
-//         if (section === 0)
-//             setShowScroll(true)
-//     }, [section])
-
-//     const handleScroll = (e) => {
-//         if (e.target.scrollTop === 0) {
-//             if (section > 0) {
-//                 setSection(pre => pre - 1)
-//                 setTimeout(() => {
-//                     e.target.scrollTop = 3;
-//                 }, 2000)
-//             }
-//         } else if (e.target.scrollTop === 6) {
-//             if (section < 6) {
-//                 setSection(pre => pre + 1)
-//                 setTimeout(() => {
-//                     e.target.scrollTop = 3;
-//                 }, 2000)
-//             }
-//         }
-
-//     }
-
-//     return (
-//         <>
-//             <div className='scrollable-div' onScroll={(e) => { handleScroll(e) }}>
-//                 {showScroll ? <div className='scrollable'></div> : <></>}
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Scrollable
+export default Scrollable
