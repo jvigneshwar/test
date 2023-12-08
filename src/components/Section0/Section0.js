ac097199990d4966f9bb8c6970fd90fa7d1db6c1
ShivaShaknr <shivanat05@gmail.com>
2023-12-08 21:27:22 +0530

    non loadig error 0

--- src/components/Section0/Section0.js
+++ src/components/Section0/Section0.js
@@ -88,7 +88,6 @@ const Section0 = ({ loadingVideo, setLoadingVideo, section, setSection, videoLoa
 
             <div className={loadingVideo ? "video-0" : "video-0-hide"}>
                 <video
-                    preload="auto"
                     ref={videoRef0}
                     muted
                     onLoadedData={() => setVideoLoaded(pre => ({...pre, "0":true}))}
